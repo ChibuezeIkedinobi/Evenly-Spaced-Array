9bb0de30293a23a28fa9d18487c12438c7ec3580
Ikedinobi <Ikdinobi@gmail.com>
2023-12-18 00:14:22 +0100

    final commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);  // scanner class to take in inputs
 
         System.out.println("Enter the first number: ");
         int a = s.nextInt();
@@ -19,9 +19,9 @@ public class Main {
     }
 
     public static boolean inputs(int a, int b, int c) {
-        int[] numbers = {a, b, c};
+        int[] numbers = {a, b, c};  // converts the passed variables to an array
 
-        Arrays.sort(numbers);
+        Arrays.sort(numbers);  //sorts the array
 
         int sum1 = numbers[1] - numbers[0];
         int sum2 = numbers[2] - numbers[1];
