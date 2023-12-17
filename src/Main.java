6809fe5a39f3c37c0e0d334e569da036424ed670
Ikedinobi <Ikdinobi@gmail.com>
2023-12-18 00:07:04 +0100

    Assignment 1

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int a = s.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int b = s.nextInt();
+
+        System.out.println("Enter the third number: ");
+        int c = s.nextInt();
+
+        System.out.println(inputs(a, b, c));
+        s.close();
+    }
+
+    public static boolean inputs(int a, int b, int c) {
+        int[] numbers = {a, b, c};
+
+        Arrays.sort(numbers);
+
+        int sum1 = numbers[1] - numbers[0];
+        int sum2 = numbers[2] - numbers[1];
+
+        return sum1 == sum2;
+    }
+}
